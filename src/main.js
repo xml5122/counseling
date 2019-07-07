420c19485c83ab28c01dad7df783bbb5b793ce50
xianyu <781047225@qq.com>
2019-07-07 16:48:21 +0800

    project init finish

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// reset.css 保持各个浏览器样式一致
+// border.css 1像素边框的解决方案
+// 解决移动端点击事件延迟3s npm install fastclick --save 引入库 --save 表示生产环境打包的时候也带上
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import './assets/css/reset.css'
+import './assets/css/border.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,14 +1,11 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div>
+    <h1>hello world</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'Home'
 }
 </script>
-
-<style>
-</style>
